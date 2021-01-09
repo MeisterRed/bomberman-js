a1ec411ef730e5583b4d2c2df019293fba559adf
jkilfoy <jordankilfoy@gmail.com>
2021-01-09 16:07:01 -0500

    initialize controller and renderer in game.js

--- static/game.js
+++ static/game.js
@@ -1,65 +1,17 @@
+// Connect to server and inform of new player
 var socket = io();
-socket.on('message', function(data) {
-    console.log(data);
-});
-
-var movement = {
-    up: false,
-    down: false,
-    left: false,
-    right: false
-}
-
-document.addEventListener('keydown', function(event) {
-    switch (event.key) {
-      case 'a': // A 
-        movement.left = true;
-        break;
-      case 'w': // W
-        movement.up = true;
-        break;
-      case 'd': // D
-        movement.right = true;
-        break;
-      case 's': // S
-        movement.down = true;
-        break;
-    }
-});
-
-document.addEventListener('keyup', function(event) {
-    switch (event.key) {
-      case 'a': // A
-        movement.left = false;
-        break;
-      case 'w': // W
-        movement.up = false;
-        break;
-      case 'd': // D
-        movement.right = false;
-        break;
-      case 's': // S
-        movement.down = false;
-        break;
-    }
-  });
-
 socket.emit('new player');
-setInterval(function() {
-    socket.emit('movement', movement);
-}, 1000 / 60);
 
-var canvas = document.getElementById('canvas');
-canvas.width = 800;
-canvas.height = 600;
-var context = canvas.getContext('2d');
-socket.on('state', function(players) {
-    context.clearRect(0, 0, 800, 600);
-    context.fillStyle = 'red';
-    for (var id in players) {
-        var player = players[id];
-        context.beginPath();
-        context.arc(player.x, player.y, 10, 0 , 2 * Math.PI);
-        context.fill();
-    }
+// Initiate controller and Renderer
+var controller = new Controller();
+var renderer = new Renderer();
+
+// Send player's actions 
+setInterval(function() {
+    socket.emit('player actions', controller.activeControls);
+}, PLAYER_ACTIONS_PER_SECOND);
+
+// Receive and render game state from the server
+socket.on('state', function(state) {
+  renderer.render(state);
 });
