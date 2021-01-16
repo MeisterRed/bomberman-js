d3138858547e607c715fade02d81f4378d2446b1
jkilfoy <jordankilfoy@gmail.com>
2021-01-16 18:49:41 -0500

    render blocks in browser

--- static/renderer.js
+++ static/renderer.js
@@ -9,10 +9,15 @@ Renderer.prototype.render = function(state) {
     var context = this.context;
     context.clearRect(0, 0, 800, 600);
     context.fillStyle = 'red';
-    for (var id in state) {
-        var player = state[id];
+    for (var id in state.players) {
+        var player = state.players[id];
         context.beginPath();
-        context.arc(player.x, player.y, 10, 0 , 2 * Math.PI);
+        context.arc(player.x+10, player.y+10, 10, 0 , 2 * Math.PI);
         context.fill();
+    }
+    context.fillStyle = 'brown';
+    for (var id in state.blocks) {
+        var block = state.blocks[id];
+        context.fillRect(block.x, block.y, 20, 20)
     }
 }
